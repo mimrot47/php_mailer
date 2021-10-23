ea06e94125df76d3680a4639079630d41cd20dfc
mimrot47 <mimrot47@gmail.com>
2021-10-23 13:52:46 +0530

    Php mailer to send email

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+include('smtp/PHPMailerAutoload.php');
+$html='Msg';
+echo smtp_mailer('mimrot47@gmail.com','subject',$html);
+function smtp_mailer($to,$subject, $msg){
+	$mail = new PHPMailer(); 
+	$mail->SMTPDebug  = 3;
+	$mail->IsSMTP(); 
+	$mail->SMTPAuth = true; 
+	$mail->SMTPSecure = 'tls'; 
+	$mail->Host = "smtp.gmail.com";
+	$mail->Port = 587; 
+	$mail->IsHTML(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->Username = "rk312151@gmail.com";
+	$mail->Password = "9922885972";
+	$mail->SetFrom("rk312151@gmail");
+	$mail->Subject = $subject;
+	$mail->Body =$msg;
+	$mail->AddAddress($to);
+	$mail->SMTPOptions=array('ssl'=>array(
+		'verify_peer'=>false,
+		'verify_peer_name'=>false,
+		'allow_self_signed'=>false
+	));
+	if(!$mail->Send()){
+		echo $mail->ErrorInfo;
+	}else{
+		return 'Sent';
+	}
+}
+?>
